87a4ca38c7fcba285e680800dd1274ff9c91c862
Franklyn Vasquez <franklynvasquez24@gmail.com>
2025-02-03 01:33:00 +0200

    Implemented connect and dispatch

--- truckconnect/connection.cpp
+++ truckconnect/connection.cpp
@@ -1,11 +1,16 @@
 #include "connection.h"
 #include "registration.h"
+#include "communication.h"
 #include <algorithm>
+#include <nstreamcom.h>
 
 using std::string;
 using std::vector;
 using std::find;
 using std::find_if;
+using nstreamcom::as_collected_size;
+using nstreamcom::nsize_int;
+using nstreamcom::encode_with_size;
 
 namespace truckconnect {
 	using pipes::pipe_handle;
@@ -15,15 +20,12 @@ namespace truckconnect {
 	vector<connection*> connection::_connections;
 
 	connection::connection()
-		: _name(""), _handle(pipe_handle()), _registrations({})
-	{
-		_connections.push_back(this);
-	}
+		: connection(nullptr, "")
+	{ }
 
 	connection::connection(pipe_handle handle, const string& name)
 		: _name(name), _handle(handle), _registrations({})
 	{
-		_source_connections.push_back(this);
 		_connections.push_back(this);
 	}
 
@@ -46,6 +48,34 @@ namespace truckconnect {
 	}
 
 	bool connection::dispatch() {
+		constexpr const size_t header_size = sizeof(channeling::telemetry_id) + sizeof(scs_u32_t);
+		vector_collector collector;
+
+		if (!pipes::try_collect(_handle, collector)) {
+			return true;
+		}
+
+		if (collector.buffer().size() < header_size) {
+			return true;
+		}
+
+		const uint8_t* data = collector.buffer().data();
+
+		const channeling::telemetry_id& id = *reinterpret_cast<const channeling::telemetry_id*>(data);
+		data += sizeof(id);
+
+		const scs_u32_t& index = *reinterpret_cast<const scs_u32_t*>(data);
+		data += sizeof(index);
+
+		if (id == communication::CLOSE) {
+			return false;
+		}
+
+		for (registration* registered : _registrations) {
+			if (registered->id() == id && registered->index() == index && registered->_callback) {
+				registered->_callback(*registered, data, collector.size() - header_size);
+			}
+		}
 		return true;
 	}
 
@@ -103,6 +133,69 @@ namespace truckconnect {
 	}
 
 	result connection::connect(connection& connection, const string& name) {
+		if (!pipes::valid_name(name)) {
+			return result::INVALID_NAME;
+		}
+
+		if (connection) {
+			return result::ALREADY_CONNECTED;
+		}
+
+		vector<uint8_t> encoded = vector<uint8_t>(
+			as_collected_size(static_cast<nsize_int>(name.size()))
+		);
+
+		encode_with_size(
+			name.begin(),
+			name.end(),
+			static_cast<nsize_int>(name.size()),
+			encoded.begin(),
+			encoded.end()
+		);
+
+		pipe_handle negotiator = pipes::open(pipes::NEGOTIATOR_PIPE_NAME);
+
+		if (!pipes::valid_handle(negotiator)) {
+			return result::INVALID_HANDLE;
+		}
+
+		if (!pipes::write(negotiator, encoded)) {
+			return result::IO_FAILURE;
+		}
+
+		vector_collector collector;
+
+		if (!pipes::try_collect(negotiator, collector)) {
+			pipes::close(negotiator);
+			return result::IO_FAILURE;
+		}
+
+		if (!communication::ack(negotiator)) {
+			pipes::close(negotiator);
+			return result::IO_FAILURE;
+		}
+
+		pipes::close(negotiator);
+
+		if (collector.size() != sizeof(result)) {
+			return result::IO_FAILURE;
+		}
+
+		result response = *reinterpret_cast<result*>(collector.buffer().data());
+
+		if (response != result::SUCCESS) {
+			return response;
+		}
+
+		pipe_handle pipe = pipes::open(name);
+
+		if (!pipes::valid_handle(pipe)) {
+			return result::PIPE_OPEN_FAILURE;
+		}
+
+		connection = truckconnect::connection(pipe, name);
+		_source_connections.push_back(&connection);
+
 		return result::SUCCESS;
 	}
 
