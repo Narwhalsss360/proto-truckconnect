ef6d251a14fc38454d2b60ff2f5862dd791c918a
Franklyn Vasquez <franklynvasquez24@gmail.com>
2025-02-02 15:55:08 +0200

    Scaffolded extension

---
+++ truckconnetextension/truckconnetextension.cpp
@@ -0,0 +1,7 @@
+#include "truckconnetextension.h"
+
+SCSAPI_RESULT scs_telemetry_init(const scs_u32_t version, const scs_telemetry_init_params_t* const params) {
+	return SCS_RESULT_ok;
+}
+
+SCSAPI_VOID scs_telemetry_shutdown() {}
---
+++ truckconnetextension/truckconnetextension.h
@@ -0,0 +1,7 @@
+#ifdef TRUCKCONNETEXTENSION_EXPORTS
+#define TRUCKCONNETEXTENSION_API __declspec(dllexport)
+#else
+#define TRUCKCONNETEXTENSION_API __declspec(dllimport)
+#endif
+
+#include "framework.h"
