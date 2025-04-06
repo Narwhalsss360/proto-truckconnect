4c04f6ebcbe776af913c5d3639e13427d0797a5f
Franklyn Vasquez <franklynvasquez24@gmail.com>
2025-04-06 19:51:01 -0400

    Added console_log() to extension framework

--- truckconnectextension/framework.h
+++ truckconnectextension/framework.h
@@ -3,4 +3,9 @@
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
 // Windows Header Files
 #include <windows.h>
+#include <string>
 #include <truckconnect.h>
+#include <scssdk.h>
+
+void console_log(scs_log_type_t type, const std::string& log);
+
--- truckconnectextension/truckconnectextension.cpp
+++ truckconnectextension/truckconnectextension.cpp
@@ -2,10 +2,13 @@
 #include "truckconnectextension.h"
 #include <scssdk_telemetry.h>
 
+using std::string;
+
+void console_log(scs_log_type_t type, const string& log) {
+
+}
+
 SCSAPI_RESULT scs_telemetry_init(const scs_u32_t version, const scs_telemetry_init_params_t* const params) {
-	using namespace truckconnect::pipes;
-	pipe_handle pipe = create(to_pipe_path(NEGOTIATOR_PIPE_NAME));
-	try_connect_to_client(pipe);
 	return SCS_RESULT_ok;
 }
 
