63dfed4ef539800aae0794e1ef625b2e05cadaf5
Franklyn Vasquez <franklynvasquez24@gmail.com>
2025-06-03 19:43:05 -0400

    Initialized dev3

---
+++ truckconnect/truckconnect.h
@@ -0,0 +1,4 @@
+#pragma once
+#include "channels.h"
+#include "communication.h"
+#include "game_data.h"
---
+++ truckconnectextension/truckconnectextension.cpp
@@ -0,0 +1,10 @@
+#include "truckconnectextension.h"
+#include <scssdk/scssdk_telemetry.h>
+
+SCSAPI_RESULT scs_telemetry_init(const scs_u32_t version, const scs_telemetry_init_params_t* const params) {
+	return SCS_RESULT_ok;
+}
+
+SCSAPI_VOID scs_telemetry_shutdown() {
+
+}
